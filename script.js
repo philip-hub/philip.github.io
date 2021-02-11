36b2564f4fd01a3734a5962695e2488ef5e85cdf
repl.it user <replituser@example.com>
2021-02-11 21:37:00 +0000

    An update

---
+++ script.js
@@ -0,0 +1,9 @@
+function cursor(){
+  //document.body.style.cursor = "not-allowed";
+  //alert("Hello you've into my website")
+}
+//context-menu
+//crosshair
+//cell
+// not-allowed
+//https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_style_cursor2
